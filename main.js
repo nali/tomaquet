a61516bb6d8bb71a2dbe2266605d6a160faa7133
Nabeelah Ali <nabeelah@typeform.com>
2016-07-08 12:31:57 +0200

    Some more luxafor stuff

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const {app, Menu, Tray} = require('electron')
+var HID = require('node-hid');
+
+// const Luxafor = require('luxafor-api')
+//
+// const device = new Luxafor({
+//   defaults: {
+//     wave: {
+//       type: 2,
+//       speed: 100,
+//       repeat: 5
+//     }
+//   }
+// })
 
 let tray = null
+
+function clickAvailable () {
+  tray.setTitle('available')
+  console.log('devices:', HID.devices());
+  // device.setColor('#FFF')
+}
+
+function clickBusy () {
+  tray.setTitle('busy')
+  // device.setColor('#FC0')
+}
 app.on('ready', () => {
   tray = new Tray('tomato.png')
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
+    {label: 'Available', type: 'radio', checked: true, click: clickAvailable},
+    {label: 'Busy', type: 'radio', click: clickBusy},
+    {label: 'Unknown', type: 'radio', click() { tray.setTitle('') }}
   ])
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
